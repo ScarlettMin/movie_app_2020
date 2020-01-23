d03bcde04ac5bfb9ac3dbf47e7d24c644e69f636
ScarlettMin <salutluvya@gmail.com>
2020-01-23 14:29:49 +0100

    Updates

---
+++ precache-manifest.1f5a1a2cca2a4955cb35872260e5fc28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96bcf88e05fe20ab3d02a0073a4eb74",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "20f3109aaa5045eb8b12",
+    "url": "/movie_app_2020/static/css/main.ba49fb35.chunk.css"
+  },
+  {
+    "revision": "9966fbb9ece634a56ad4",
+    "url": "/movie_app_2020/static/js/2.44bdb5fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.44bdb5fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20f3109aaa5045eb8b12",
+    "url": "/movie_app_2020/static/js/main.3a6264f9.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
